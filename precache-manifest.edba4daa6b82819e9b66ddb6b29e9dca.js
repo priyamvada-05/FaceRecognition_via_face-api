4f9848e408c103a4beca8c82c731cb5e6131d402
priyamvada <pranshulpandey5@gmail.com>
2019-11-16 00:24:06 +0000

    Updates

---
+++ precache-manifest.edba4daa6b82819e9b66ddb6b29e9dca.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ca5289bad31df2e693f5fb798f77635",
+    "url": "/FaceRecognition_via_face-api/index.html"
+  },
+  {
+    "revision": "91bd376a30b598439a3d",
+    "url": "/FaceRecognition_via_face-api/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "fa2ee1b3fe3ca4b7987b",
+    "url": "/FaceRecognition_via_face-api/static/css/main.60e6d85a.chunk.css"
+  },
+  {
+    "revision": "91bd376a30b598439a3d",
+    "url": "/FaceRecognition_via_face-api/static/js/2.84a092d8.chunk.js"
+  },
+  {
+    "revision": "fa2ee1b3fe3ca4b7987b",
+    "url": "/FaceRecognition_via_face-api/static/js/main.44040cce.chunk.js"
+  },
+  {
+    "revision": "7e9bb9565b1d0cd10c79",
+    "url": "/FaceRecognition_via_face-api/static/js/runtime-main.c21e94d8.js"
+  },
+  {
+    "revision": "12f9bb12fbe355e1acc2a0cab0ca2ec1",
+    "url": "/FaceRecognition_via_face-api/static/media/Kingthings Pique n meex.12f9bb12.woff"
+  },
+  {
+    "revision": "5301fbedc1bf5fa88edb6ff5bd355b50",
+    "url": "/FaceRecognition_via_face-api/static/media/test_img.5301fbed.jpeg"
+  }
+]);
